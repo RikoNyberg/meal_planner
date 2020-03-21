020c985dacfcaf500d9b121956396bf721a3c159
Riko Nyberg <riko.nyberg@aalto.fi>
2020-03-22 00:43:14 +0100

    Updated algorithm structure

--- fourkind/algorithm.py
+++ fourkind/algorithm.py
@@ -5,41 +5,95 @@ from scipy.optimize import linprog
 # import logging
 import data_import
 
-_, df_clean, _ = data_import.get_data()
-df = df_clean
+class DailyMealPlan():
+    def __init__(self, meals, meal_limitations={}, prev_day_meal_plan=None, day='21-3-20'):
+        self.daily_meal_plan_calculated = False
+        self.df = meals
+        limits = meal_limitations
+        self.prev_meal_plan = prev_day_meal_plan
+        self.day = day
 
-fibre = df['fibre']
-sugar = df['sugar']
-kcal = df['kcal']
+        def get_limit(limit_name, default, limits_dict):
+            return default if not limits_dict.get(limit_name) else limits_dict[limit_name]
 
-carb_kcal = df['carb_kcal']
-protein_kcal = df['protein_kcal']
-fat_kcal = df['fat_kcal']
+        # Mandatory
+        self.fibre_limit = get_limit('fibre_limit', 20, limits)
+        self.kcal_limit = get_limit('kcal_limit', 2000, limits)
+        self.carb_kcal_limit = get_limit('carb_kcal_limit', 1000, limits)
+        self.protein_kcal_limit = get_limit('protein_kcal_limit', 600, limits)
+        self.fat_kcal_limit = get_limit('fat_kcal_limit', 400, limits)
 
-# Extra
-salt = df['salt'] # (mg)
-sodium = df['sodium'] # (mg)
+        # Extra
+        self.salt_limit = get_limit('salt_limit', 5000, limits)
+        self.sodium_limit = get_limit('sodium_limit', 1500, limits)
+
+    def calculate_optimal_meal_plan(self):
+
+        # self.prev_meal_plan.get_optimal_meal_plan()[['category','extra_categoty']]
+
+        # Mandatory
+        sugar = self.df['sugar']
+        fibre = self.df['fibre']
+        kcal = self.df['kcal']
+        carb_kcal = self.df['carb_kcal']
+        protein_kcal = self.df['protein_kcal']
+        fat_kcal = self.df['fat_kcal']
+
+        # Extra
+        salt = self.df['salt'] # (mg)
+        sodium = self.df['sodium'] # (mg)
+
+        A_upperbounds = np.array([-fibre, sodium, salt])
+        b_upperbounds = np.array([
+            -self.fibre_limit, self.sodium_limit, self.salt_limit
+            ])
+        A_equality = np.array([kcal, carb_kcal, protein_kcal, fat_kcal])
+        b_equality = np.array([
+            self.kcal_limit, self.carb_kcal_limit, self.protein_kcal_limit, self.fat_kcal_limit
+            ])
+        bounds = [(0, 5) for x in range(self.df.shape[0])] # 50 - 500g
+
+        solution = linprog(
+            c=sugar,
+            A_ub=A_upperbounds,
+            b_ub=b_upperbounds,
+            A_eq=A_equality,
+            b_eq=b_equality,
+            bounds=bounds
+        )
+        self.df['grams'] = solution.x[solution.x > 0] * 100
+        self.daily_meal_plan_calculated = True
+
+    def get_optimal_meal_plan(self):
+        if not self.daily_meal_plan_calculated:
+            # logger.info("Optimizing today's meal plan...")
+            self.calculate_optimal_meal_plan()
+        info = ['name', 'grams', 'kcal', 'sugar', 'fibre', 'carb_kcal', 'protein_kcal', 'fat_kcal', 'salt', 'sodium', 'category', 'extra_category']
+        df = self.df[info].copy()
+
+        # Calculating meal plans nutrient quantities for each food
+        nutrients = ['fibre', 'sugar', 'kcal', 'carb_kcal', 'protein_kcal', 'fat_kcal', 'salt', 'sodium']
+        df_grams = df['grams'].div(100)
+        df[nutrients] = df[nutrients].multiply(df_grams, axis='index') # NOTE: each nutrient is per 100g
+        df.sort_values(by='grams', ascending=False, inplace=True)
+        
+        return df[df['grams'] >= 0.1]
+
+    def get_total_nutrients(self):
+        columns = ['grams', 'fibre', 'sugar', 'kcal', 'carb_kcal', 'protein_kcal', 'fat_kcal', 'salt', 'sodium']
+        return self.get_optimal_meal_plan()[columns].sum()
+
+    def save_total_nutrients_to_csv(self):
+        self.get_total_nutrients().to_csv(f'{self.day}_nutrients.csv')
+
+    def save_meal_plan_to_csv(self):
+        self.get_optimal_meal_plan().to_csv(f'{self.day}_meal_plan.csv')
 
 
-A_upperbounds = np.array([-fibre, sodium, salt])
-b_upperbounds = np.array([-20, 1500, 5000])
-A_equality = np.array([kcal, carb_kcal, protein_kcal, fat_kcal])
-b_equality = np.array([2000, 1000, 600, 400])
-bounds = [(0, 5) for x in range(df.shape[0])]
 
-solution = linprog(
-    c=sugar,
-    A_ub=A_upperbounds,
-    b_ub=b_upperbounds,
-    A_eq=A_equality,
-    b_eq=b_equality,
-    bounds=bounds
-)
-
-print(solution.x[solution.x > 0])
-print(solution)
-
-df['optimal_amount'] = solution.x[solution.x > 0]
-essentials = ['name', 'optimal_amount', 'kcal', 'sugars, total (g)', 'energy,calculated (kJ)','fat, total (g)', 'carbohydrate, available (g)', 'protein, total (g)', 'fibre, total (g)', 'kcal_ratio', 'sodium', 'salt']
-df = df[essentials]
-df.to_csv('result.csv')
+df = data_import.get_data()
+df = data_import.clean_data(df)
+saturday = DailyMealPlan(df, day='saturday')
+print(saturday.get_optimal_meal_plan())
+saturday.save_meal_plan_to_csv()
+saturday.save_total_nutrients_to_csv()
--- fourkind/data_import.py
+++ fourkind/data_import.py
@@ -6,54 +6,68 @@ from pandas.api.types import is_string_dtype, is_numeric_dtype
 
 def get_data():
     df = pd.read_csv('../resultset.csv', sep=";")
-    columns = get_columns()
-    df = df[columns]
-    # df['categoty'], df['info'], df['product'] = df['name'].str.split(', ', 2).str # NOTE: This is dangerous because it creates several NaN values
-    # df.drop('name', axis=1, inplace=True)
-    df, df_clean, df_no_nan = clean_data(df)
-    # logging.info('Data imported and cleaned')
-    return df, df_clean, df_no_nan
+    wanted_columns = get_columns()
+    df = df[wanted_columns]
+    df = set_columns(df)
 
-def ensure_format(df, col_name):
-    if is_string_dtype(df[col_name]):
-        return pd.to_numeric(df[col_name].map(lambda x: x if x != '<0.1' else 0 )) # NOTE: the estimate <0.1 creates a distortion in the data. Hence, removed.
-    elif is_numeric_dtype(df[col_name]):
-        return df[col_name]
-    else:
-        raise Exception('Data is not correct format (number or string)')
+    # logging.info('Data imported and cleaned')
+    return df
+
 
 def clean_column(df, col_name):
     if is_string_dtype(df[col_name]):
         col = pd.to_numeric(df[col_name].map(lambda x: x if x != '<0.1' else 0 )) # NOTE: the estimate <0.1 creates a distortion in the data. Hence, removed.
+    else:
+        col = df[col_name]
     return col.replace([np.inf, -np.inf], np.nan).fillna(0)
 
+def get_extra_category_list(df):
+    """
+    These are more general categories to improve the variety of meal plans.
+    Note that short category names might get unwanted matches as the matching is really simple currently.
+    E.g. 'ham' --> 'ham|burger' or 'c|ham|pignon soup'.
+    Hence, be careful with these.
+    """
+    extra_categories = ['chicken', 'pork', 'beef', 'kebab', 'fish', 'cheese', 'shrimp', 'porridge', 'hamburger', ' ham', 'ham ', 'pasta', 'salmon', 'cake', 'lamb', 'tofu']
+    extra_category_list = []
 
-def clean_data(df):
-    df['kcal'] = ensure_format(df, 'energy,calculated (kJ)') / 4.184 # NOTE: kJ -> kcal
-    df['fat_kcal'] = ensure_format(df, 'fat, total (g)') * 9
-    df['carb_kcal'] = ensure_format(df, 'carbohydrate, available (g)') * 4
-    df['protein_kcal'] = ensure_format(df, 'protein, total (g)') * 4
-    df['alc_kcal'] = ensure_format(df, 'alcohol (g)') * 7
-    df['sugar'] = ensure_format(df, 'sugars, total (g)')
-    df['fibre'] = ensure_format(df, 'fibre, total (g)')
-    df['alc'] = ensure_format(df, 'alcohol (g)')
+    def get_extra_category_value(row):
+        for extra_c in extra_categories:
+            if extra_c in row['name']:
+                return extra_c
+        return ''
+
+    for i, row in df.iterrows():
+        extra_category_list.append(get_extra_category_value(row))
+    return extra_category_list
+
+
+
+def set_columns(df):
+    df['category'], _, _ = df['name'].str.split(', ', 2).str
+    df['extra_category'] = get_extra_category_list(df)
+
+    df['kcal'] = clean_column(df, 'energy,calculated (kJ)') / 4.184 # NOTE: kJ -> kcal
+    df['fat_kcal'] = clean_column(df, 'fat, total (g)') * 9
+    df['carb_kcal'] = clean_column(df, 'carbohydrate, available (g)') * 4
+    df['protein_kcal'] = clean_column(df, 'protein, total (g)') * 4
+    df['alc_kcal'] = clean_column(df, 'alcohol (g)') * 7
+    df['sugar'] = clean_column(df, 'sugars, total (g)')
+    df['fibre'] = clean_column(df, 'fibre, total (g)')
+    df['alc'] = clean_column(df, 'alcohol (g)')
     df['sodium'] = clean_column(df, 'sodium (mg)')
     df['salt'] = clean_column(df, 'salt (mg)')
+
     df['kcal_ratio'] = (df['kcal'] / (df['fat_kcal'] + df['carb_kcal'] + df['protein_kcal'] + df['alc_kcal']))
-    df_no_nan = df.replace([np.inf, -np.inf], np.nan).dropna(subset=["kcal_ratio"])
-    df_clean = df_no_nan[df_no_nan.kcal_ratio <= 1.3]
 
-    # df['kj'] = ensure_format(df, 'energy,calculated (kJ)')
-    # df['fat_kj'] = ensure_format(df, 'fat, total (g)') * 38
-    # df['carb_kj'] = ensure_format(df, 'carbohydrate, available (g)') * 17
-    # df['protein_kj'] = ensure_format(df, 'protein, total (g)') * 17
-    # df['alc_kj'] = ensure_format(df, 'alcohol (g)') * 30
-    # df['kj_ratio'] = (df['kj'] / (df['fat_kj'] + df['carb_kj'] + df['protein_kj'] + df['alc_kj']))
-    # df_no_nan = df.replace([np.inf, -np.inf], np.nan).dropna(subset=["kj_ratio"])
-    # df_clean = df_no_nan[df_no_nan.kj_ratio <= 1.3]
+    return df
 
+def clean_data(df):
+    """Cleaning from NaN, inf, and outliers"""
+    df_no_nan = df.copy().replace([np.inf, -np.inf], np.nan).dropna(subset=["kcal_ratio"])
+    df_clean = df_no_nan.copy()[df_no_nan.kcal_ratio <= 1.3] # No outliers
     df_clean = df_clean[df_clean.alc <= 0.4]
-    return df, df_clean, df_no_nan
+    return df_clean
 
 def get_columns():
     
