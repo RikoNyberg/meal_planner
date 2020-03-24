9eaf260ac11acbcba6bec05fda141471d24dd3a0
Riko Nyberg <riko.nyberg@aalto.fi>
2020-03-24 19:21:36 +0100

    Unittests and data analytic updates

--- tests/run_data_analytics.py
+++ tests/run_data_analytics.py
@@ -2,72 +2,91 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import sys
-sys.path.insert(0,'..')
+sys.path.insert(0, '..')
 from fourkind import data_import
 
+
 df = data_import.get_data()
-df_no_nan = df.copy().replace([np.inf, -np.inf], np.nan).dropna(subset=["kcal_ratio"])
+df_no_nan = df.copy().replace([np.inf, -np.inf],
+                              np.nan).dropna(subset=["kcal_ratio"])
 df = df.sort_values('kcal_ratio')
 df_no_nan = df_no_nan.sort_values('kcal_ratio')
 df_no_nan.reset_index(drop=True, inplace=True)
 
 
-print('-INFO- Ratio of food that has informed energy within -10% to +30%',
-    'of the calculated energy sum of macronutrients: {}%'.format(
-        round(len(df_no_nan[df_no_nan.kcal_ratio <= 1.3]) / len(df) * 100, 1)))
+print('\nRatio of food that informed energy within -10% to +30%',
+      'of the calculated energy sum of its macronutrients: {}%\n'.format(
+          round(len(df_no_nan[df_no_nan.kcal_ratio <= 1.3]) / len(df) * 100, 1)))
+
 
 def save_outliers_to_csv(df_orig):
-    # Save the significant outliers as csv to get a picture what kind of outliers exist:
+    '''Save the significant outliers as csv to get a picture what kind of outliers exist'''
     df = df_orig.copy()
     df_outliers = df[df.kcal_ratio > 1.3]
     df_nan = df[df.isin([np.nan, np.inf, -np.inf]).any(1)]
     df_outliers = pd.concat([df_outliers, df_nan])
-    essentials = ['name', 'energy,calculated (kJ)','fat, total (g)', 'carbohydrate, available (g)', 'protein, total (g)', 'fibre, total (g)', 'sugars, total (g)', 'alcohol (g)']
-    df_outliers[essentials + ['kcal_ratio']].to_csv('./assets/analytics_of_input_data/energy_vs_macronutrients-outliers.csv')
-    print('-INFO- Outlier count (not sufficien info or over +30%): {}/{}'.format(len(df_outliers), len(df)))
+    essentials = ['name', 'energy,calculated (kJ)', 'fat, total (g)', 'carbohydrate, available (g)',
+                  'protein, total (g)', 'fibre, total (g)', 'sugars, total (g)', 'alcohol (g)']
+    path = './assets/analytics_of_input_data/energy_vs_macronutrients-outliers.csv'
+    df_outliers[essentials + ['kcal_ratio']].to_csv(path)
+    print('Outlier count (not sufficien info or over +30%): {}/{}'.format(len(df_outliers), len(df)))
+    print(f'Saved "energy_vs_macronutrients-outliers" chart to {path}\n')
 
 
 def save_energy_difference_distribution(df_no_nan):
-    # Save the distribution of informed Energy and Sum of macronutrients energy difference
-    # to get a picture what kind of how much outliers exist:
+    '''
+    Save the distribution of informed Energy and Sum of macronutrients energy difference
+    to get a picture how much outliers exist
+    '''
     plt.clf()
     plot = df_no_nan[['kcal_ratio']].plot(
         kind='hist',
         bins=[x * 0.1 for x in range(5, 20)],
         title='Distribution of informed Energy and Sum of macronutrients energy difference'
-        )
+    )
     plot.set_xlabel('Energy / Sum of macronutrients')
     plt.legend().remove()
-    plot.get_figure().savefig('assets/analytics_of_input_data/energy_vs_macronutrients-ratio_0-2.png')
+    path = 'assets/analytics_of_input_data/energy_vs_macronutrients-ratio_0-2.png'
+    plot.get_figure().savefig(path, bbox_inches='tight')
+    print(f'Saved "Energy / Sum of macronutrients" chart to {path}\n')
 
 
 def print_category_size_distributions(df):
     category_dict = get_product_category_dict(df)
     print('Categories in total:', len(category_dict))
-    for n in range(1,6):
+    for n in range(1, 6):
         count = 0
         for _, value in category_dict.items():
             count += 1 if value <= n else 0
         print('Food categories with {} or less food items:'.format(n), count)
+    print()
 
 
 def print_extra_category_counts(df):
     category_dict = get_product_category_dict(df)
-    for category in ['chicken', 'pork', 'beef', 'ham', 'kebab', 'fish', 'cheese', 'shrimp', 'porridge', 'hamburger', 'pasta', 'salmon', 'cake', 'lamb', 'tofu']: # TODO: for cakes could create 2 categories for salty and sweet
+    # TODO: for cakes could create 2 categories for salty and sweet
+    for category in ['chicken', 'pork', 'beef', 'ham', 'kebab', 'fish', 'cheese', 'shrimp', 'porridge', 'hamburger', 'pasta', 'salmon', 'cake', 'lamb', 'tofu']:
         food_count = 0
         categoty_count = 0
         for key, value in category_dict.items():
             food_count += value if (category in key.lower()) else 0
             categoty_count += 1 if (category in key.lower()) else 0
-        print('Food categories {} and {} foods with {}'.format(categoty_count, food_count, category))
+        print('Food categories {} and {} foods with {}'.format(
+            categoty_count, food_count, category))
+    print()
+
 
 def save_distribution_of_ingredient_count_per_category(df):
     category_dict = get_product_category_dict(df)
     plt.clf()
-    plt.bar(range(len(category_dict)), list(category_dict.values()), align='center')
+    plt.bar(range(len(category_dict)), list(
+        category_dict.values()), align='center')
     plt.xlabel('Count of categories')
     plt.ylabel('Count of ingredients per category')
-    plt.savefig('assets/analytics_of_input_data/ingredient_count_per_category.png', bbox_inches='tight')
+    path = 'assets/analytics_of_input_data/ingredient_count_per_category.png'
+    plt.savefig(path, bbox_inches='tight')
+    print(f'Saved "Count of ingredients per category" chart to {path}\n')
+
 
 def get_product_category_dict(df):
     cat_dict = {}
@@ -76,8 +95,10 @@ def get_product_category_dict(df):
             cat_dict[category] = 1
         else:
             cat_dict[category] += 1
-    category_dict = {k: v for k, v in sorted(cat_dict.items(), key=lambda item: item[1])}
+    category_dict = {k: v for k, v in sorted(
+        cat_dict.items(), key=lambda item: item[1])}
     return category_dict
+
 
 save_outliers_to_csv(df)
 save_energy_difference_distribution(df_no_nan)
---
+++ tests/test_data_import.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import unittest
+import pandera as pa
+
+import sys
+sys.path.insert(0, '..')
+from fourkind import data_import
+
+
+class TestDataImport(unittest.TestCase):
+
+    def test_csv_download(self):
+        df = data_import.download_csv()
+        schema_csv_download = pa.DataFrameSchema({
+            'name': pa.Column(pa.String),
+            'energy,calculated (kJ)': pa.Column(pa.Int, pa.Check(
+                lambda x: 0 <= x <= 4000, element_wise=True,
+                error="kJ range checker [0, 2000]")),
+            'fat, total (g)': pa.Column(pa.String),
+            'carbohydrate, available (g)': pa.Column(pa.String),
+            'protein, total (g)': pa.Column(pa.String),
+            # 'fibre, total (g)': pa.Column(), # can have NaN values
+            'sugars, total (g)': pa.Column(pa.String),
+            'alcohol (g)': pa.Column(pa.String),
+            # 'sodium (mg)': pa.Column(), # can have NaN values
+            'salt (mg)': pa.Column(pa.String),
+        })
+        df_valid = schema_csv_download.validate(df)
+        self.assertTrue(1000 in df_valid.index)
+
+    def test_get_extra_category_list(self):
+        extra_categories_test = ['chicken', 'pork', 'beef', '', 'kebab', 'fish', 'cheese', 'shrimp',
+                                 'porridge', 'lamb', 'tofu', 'salmon', 'hamburger', ' ham', 'ham ', 'pasta', 'cake', 'rice', 'protein']
+        df_test = pd.DataFrame(extra_categories_test, columns=['name'])
+        extra_cat_created_list = data_import.get_extra_category_list(df_test)
+
+        self.assertEqual(len(df_test), len(extra_cat_created_list))
+        self.assertEqual(extra_categories_test, extra_cat_created_list)
+
+    def test_add_necessary_columns(self):
+        data = {
+            'name': ['Tämä on Ruuan kategoria, ja tämä tuotemerkki Beef, ja tää on detail'],
+            'energy,calculated (kJ)': [123],
+            'fat, total (g)': ['3.4'],
+            'carbohydrate, available (g)': ['58.8'],
+            'protein, total (g)': ['<0.1'],
+            'sugars, total (g)': ['1.3'],
+            'fibre, total (g)': ['11.5'],
+            'alcohol (g)': ['0.0'],
+            'sodium (mg)': ['87.8'],
+            'salt (mg)': ['470.1'],
+        }
+        df_test = pd.DataFrame(data)
+        df = data_import.add_necessary_columns(df_test)
+
+        schema_added_columns = pa.DataFrameSchema({
+            'category': pa.Column(pa.String),
+            'extra_category': pa.Column(pa.String),
+            'kcal': pa.Column(pa.Float, pa.Check(lambda s: s >= 0)),
+            'fat_kcal': pa.Column(pa.Float, pa.Check(lambda s: s >= 0)),
+            'carb_kcal': pa.Column(pa.Float, pa.Check(lambda s: s >= 0)),
+            'protein_kcal': pa.Column(pa.Float, pa.Check(lambda s: s >= 0)),
+            'alc_kcal': pa.Column(pa.Float, pa.Check(lambda s: s >= 0)),
+            'sugar': pa.Column(pa.Float, pa.Check(lambda s: s >= 0)),
+            'fibre': pa.Column(pa.Float, pa.Check(lambda s: s >= 0)),
+            'alc': pa.Column(pa.Float, pa.Check(lambda s: s >= 0)),
+            'sodium': pa.Column(pa.Float, pa.Check(lambda s: s >= 0)),
+            'salt': pa.Column(pa.Float, pa.Check(lambda s: s >= 0)),
+            'kcal_ratio': pa.Column(pa.Float, pa.Check(lambda s: s >= 0)),
+        })
+
+        df_valid = schema_added_columns.validate(df)
+
+        self.assertEqual(df_valid['kcal'][0],
+                         df_test['energy,calculated (kJ)'][0]/4.184)
+        self.assertEqual(df_valid['salt'][0], 470.1)
+        self.assertEqual(df_valid['category'][0], 'Tämä on Ruuan kategoria')
+        self.assertEqual(df_valid['extra_category'][0], 'beef')
+
+
+if __name__ == '__main__':
+    unittest.main()
